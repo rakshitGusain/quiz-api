b65f1067bec8420681f571b395da16b10fdc7fee
rakshitGusain <76807660+rakshitGusain@users.noreply.github.com>
2023-04-20 01:58:27 +0530

    Create quiz.model.js

---
+++ models/quiz.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+// const { stringify } = require("querystring");
+
+const quizSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: [String],
+    required: true,
+  },
+  rightAnswer: {
+    type: Number,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "inactive",
+  },
+});
+
+module.exports = mongoose.model("Quiz", quizSchema);
